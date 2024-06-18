3b9606052faa8e61415aaddbe549205835e1b98a
root <johnasking@gmail.com>
2024-06-18 15:22:07 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
-[200~#!/usr/bin/node
+#!/usr/bin/node
 
-	if (process.argv.length === 2) {
-		  console.log('No argument');
-	} else if (process.argv.length === 3) {
-		  console.log('Argument found');
-	} else {
-		  console.log('Arguments found');
-	}
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
