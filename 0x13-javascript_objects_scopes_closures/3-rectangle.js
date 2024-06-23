713cba8132ea67188c33a13f2b1bfe7ed4c84e7f
root <johansking959@gmail.com>
2024-06-23 02:03:05 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,17 @@
-#!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += 'X';
-      }
-      console.log(s);
-    }
-  }
-}
 
-module.exports = Rectangle;
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,29 @@
-#!/usr/bin/node
+#!/usr/bin/python3
+"""
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
 
-exports.esrever = function (list) {
-  const revList = [];
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-  for (let i = list.length - 1; i >= 0; --i) {
-    revList.push(list[i]);
-  }
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-  return revList;
-};
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    instance = session.query(State).order_by(State.id).first()
+
+    if instance is None:
+        print('Nothing')
+    else:
+        print('{0}: {1}'.format(instance.id, instance.name))
